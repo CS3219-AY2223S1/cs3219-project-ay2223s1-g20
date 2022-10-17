d4cec3057f5a8b080c8e854315c002b37fb538b2
Goh Siau Chiak <gsc928@gmail.com>
2022-10-17 16:48:27 +0800

    Dockerize question service

--- question-service/index.js
+++ question-service/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { FieldValue } = require('firebase-admin/firestore');
 
 const app = express()
-const port = 8383
+const port = process.env.port || 8003;
 const { db } = require('./firebase-config.js');
 const { application } = require("express");
 
