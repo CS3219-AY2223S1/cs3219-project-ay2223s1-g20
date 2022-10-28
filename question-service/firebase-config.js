b68fd99600bb475a55f38c70a8850be851210b2c
Goh Siau Chiak <gsc928@gmail.com>
2022-10-28 17:13:46 +0800

    Update question service firebase creds

--- question-service/firebase-config.js
+++ question-service/firebase-config.js
@@ -1,15 +1,14 @@
 const { initializeApp, cert } = require('firebase-admin/app')
 const { getFirestore } = require('firebase-admin/firestore')
+const { readFileSync } = require('fs')
 
-let credentials
+const service_account = require('./creds.json')
+const key = process.env.FIREBASE_KEY || readFileSync('./firebase.key').toString()
 
-if (process.env.FIREBASE_CONFIG != undefined) {
-    credentials = JSON.parse(process.env.FIREBASE_CONFIG)
-} else {
-    credentials = require('./creds.json')
-}
-
-initializeApp(credentials)
+initializeApp({
+    ...service_account,
+    privateKey: key,
+})
 
 const db = getFirestore()
 
