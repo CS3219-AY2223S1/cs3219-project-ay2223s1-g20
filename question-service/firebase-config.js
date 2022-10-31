c36d6f4aedea91b1db2822eff7a31a3ba138e96c
Goh Siau Chiak <gsc928@gmail.com>
2022-10-31 17:46:00 +0800

    Fix firebase auth

--- question-service/firebase-config.js
+++ question-service/firebase-config.js
@@ -2,10 +2,12 @@ const { credential } = require('firebase-admin')
 const { initializeApp } = require('firebase-admin/app')
 const { getFirestore } = require('firebase-admin/firestore')
 
+require('dotenv').config()
+
 const serviceAccount = require('./creds.json')
 const privateKey = process.env.FIREBASE_KEY.replace(/\\n/g, '\n')
 
-initializeApp({ credential: credential.cert({ ...serviceAccount, "private_key": privateKey.substring(1, privateKey.length - 1) }) })
+initializeApp({ credential: credential.cert({ ...serviceAccount, "private_key": privateKey }) })
 
 const db = getFirestore()
 
