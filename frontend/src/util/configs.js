842cf50f99e9248697dcfb0d62fc0967db7d8367
Goh Siau Chiak <gsc928@gmail.com>
2022-11-01 01:49:13 +0800

    Update frontend svc prefixes

--- frontend/src/util/configs.js
+++ frontend/src/util/configs.js
@@ -1,13 +1,13 @@
-export const USER_SVC_PREFIX = '/user'
+export const USER_SVC_PREFIX = process.env.ENV === 'PROD' ? process.env.URI_USER_SVC : '/user'
 export const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000'
 
-export const MATCHING_SVC_PREFIX = '/matching'
+export const MATCHING_SVC_PREFIX = process.env.ENV === 'PROD' ? process.env.URI_MATCHING_SVC : '/matching'
 export const URI_MATCHING_SVC = process.env.URI_MATCHING_SVC || 'http://localhost:8001'
 
-export const COLLAB_SVC_PREFIX = '/collab'
+export const COLLAB_SVC_PREFIX = process.env.ENV === 'PROD' ? process.env.URI_COLLAB_SVC : '/collab'
 export const URI_COLLAB_SVC = process.env.URI_COLLAB_SVC || 'http://localhost:8002'
 
-export const QUESTION_SVC_PREFIX = '/question'
+export const QUESTION_SVC_PREFIX = process.env.ENV === 'PROD' ? process.env.URI_QUESTION_SVC : '/question'
 export const URI_QUESTION_SVC = process.env.URI_QUESTION_SVC || 'http://localhost:8003'
 
 export const REGISTER = '/accounts'
