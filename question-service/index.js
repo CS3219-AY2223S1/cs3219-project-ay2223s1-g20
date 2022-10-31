4c5a6cc4b5a74b97a01d630292c134fcf807bcbe
Goh Siau Chiak <gsc928@gmail.com>
2022-10-31 22:31:31 +0800

    Update index.js

--- question-service/index.js
+++ question-service/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { FieldValue } = require('firebase-admin/firestore');
 
 const app = express()
-const port = process.env.port || 8003;
+const port = process.env.PORT || 8003;
 const { db } = require('./firebase-config.js');
 const { application } = require("express");
 
