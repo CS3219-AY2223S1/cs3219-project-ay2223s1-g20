806a9ca02ab421da1553f56ecefcd4790cbc57a6
Goh Siau Chiak <gsc928@gmail.com>
2022-10-31 22:15:56 +0800

    Update firebase-config.js

--- question-service/firebase-config.js
+++ question-service/firebase-config.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const serviceAccount = require('./creds.json')
 const privateKey = process.env.FIREBASE_KEY.replace(/\\n/g, '\n')
 
-initializeApp({ credential: credential.cert({ ...serviceAccount, "private_key": privateKey.substring(1, privateKey.length - 1) }) })
+initializeApp({ credential: credential.cert({ ...serviceAccount, "private_key": privateKey }) })
 
 const db = getFirestore()
 
