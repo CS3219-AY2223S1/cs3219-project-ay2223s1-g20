9f7596ecca906935564e28037e4156bc923c7e38
Goh Siau Chiak <gsc928@gmail.com>
2022-11-01 09:12:06 +0800

    Change frontend env var names

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/user', {
-      target: process.env.URI_USER_SVC || 'http://localhost:8000', // User Service
+      target: process.env.REACT_APP_URI_USER_SVC || 'http://localhost:8000', // User Service
       changeOrigin: true,
       pathRewrite: {
         '^/user': ''
@@ -15,7 +15,7 @@ module.exports = function (app) {
   )
   app.use(
     createProxyMiddleware('/matching', {
-      target: process.env.URI_MATCHING_SVC || 'http://localhost:8001', // Matching Service
+      target: process.env.REACT_APP_URI_MATCHING_SVC || 'http://localhost:8001', // Matching Service
       changeOrigin: true,
       pathRewrite: {
         '^/matching': ''
@@ -27,7 +27,7 @@ module.exports = function (app) {
   )
   app.use(
     createProxyMiddleware('/collab', {
-      target: process.env.URI_COLLAB_SVC || 'http://localhost:8002', // Collaboration Service
+      target: process.env.REACT_APP_URI_COLLAB_SVC || 'http://localhost:8002', // Collaboration Service
       changeOrigin: true,
       pathRewrite: {
         '^/collab': ''
@@ -39,7 +39,7 @@ module.exports = function (app) {
   )
   app.use(
     createProxyMiddleware('/question', {
-      target: process.env.URI_QUESTION_SVC || 'http://localhost:8003', // Question Service
+      target: process.env.REACT_APP_URI_QUESTION_SVC || 'http://localhost:8003', // Question Service
       changeOrigin: true,
       pathRewrite: {
         '^/question': ''
--- frontend/src/util/configs.js
+++ frontend/src/util/configs.js
@@ -1,20 +1,20 @@
-console.log(process.env.ENV)
-console.log(process.env.URI_USER_SVC)
-console.log(process.env.URI_MATCHING_SVC)
-console.log(process.env.URI_COLLAB_SVC)
-console.log(process.env.URI_QUESTION_SVC)
+console.log(process.env.REACT_APP_ENV)
+console.log(process.env.REACT_APP_URI_USER_SVC)
+console.log(process.env.REACT_APP_URI_MATCHING_SVC)
+console.log(process.env.REACT_APP_URI_COLLAB_SVC)
+console.log(process.env.REACT_APP_URI_QUESTION_SVC)
 
-export const USER_SVC_PREFIX = process.env.ENV === 'PROD' ? process.env.URI_USER_SVC : '/user'
-export const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000'
+export const USER_SVC_PREFIX = process.env.REACT_APP_ENV === 'PROD' ? process.env.REACT_APP_URI_USER_SVC : '/user'
+export const URI_USER_SVC = process.env.REACT_APP_URI_USER_SVC || 'http://localhost:8000'
 
-export const MATCHING_SVC_PREFIX = process.env.ENV === 'PROD' ? process.env.URI_MATCHING_SVC : '/matching'
-export const URI_MATCHING_SVC = process.env.URI_MATCHING_SVC || 'http://localhost:8001'
+export const MATCHING_SVC_PREFIX = process.env.REACT_APP_ENV === 'PROD' ? process.env.REACT_APP_URI_MATCHING_SVC : '/matching'
+export const URI_MATCHING_SVC = process.env.REACT_APP_URI_MATCHING_SVC || 'http://localhost:8001'
 
-export const COLLAB_SVC_PREFIX = process.env.ENV === 'PROD' ? process.env.URI_COLLAB_SVC : '/collab'
-export const URI_COLLAB_SVC = process.env.URI_COLLAB_SVC || 'http://localhost:8002'
+export const COLLAB_SVC_PREFIX = process.env.REACT_APP_ENV === 'PROD' ? process.env.REACT_APP_URI_COLLAB_SVC : '/collab'
+export const URI_COLLAB_SVC = process.env.REACT_APP_URI_COLLAB_SVC || 'http://localhost:8002'
 
-export const QUESTION_SVC_PREFIX = process.env.ENV === 'PROD' ? process.env.URI_QUESTION_SVC : '/question'
-export const URI_QUESTION_SVC = process.env.URI_QUESTION_SVC || 'http://localhost:8003'
+export const QUESTION_SVC_PREFIX = process.env.REACT_APP_ENV === 'PROD' ? process.env.REACT_APP_URI_QUESTION_SVC : '/question'
+export const URI_QUESTION_SVC = process.env.REACT_APP_URI_QUESTION_SVC || 'http://localhost:8003'
 
 export const REGISTER = '/accounts'
 export const LOG_IN = '/login'
