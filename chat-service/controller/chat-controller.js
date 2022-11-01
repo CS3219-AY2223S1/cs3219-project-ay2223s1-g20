24adc3e44956221b9f3c753c168824aa7249e105
xuanqi966 <xuanqi.wang966@gmail.com>
2022-11-01 22:40:20 +0800

    modify event data format

--- chat-service/controller/chat-controller.js
+++ chat-service/controller/chat-controller.js
@@ -18,10 +18,11 @@ export async function handleChatEvents(io) {
             }
         })
 
-        socket.on('sendMessage', async (message) => {
+        socket.on('sendMessage', async (req) => {
             console.log(`[socketIO, sendMessage] socketId=${socket.id}`)
+            console.log("[socketIO, sendMessage] req=", req)
             const chatId = await getChatId(socket.id)
-            io.to(chatId).emit('newMessage', message)
+            io.to(chatId).emit('newMessage', req)
         })
 
         socket.on('leaveRoom', async () => {
