12bba15e4c03fca2be50c947e392d193949ad4e2
MarcusTan <marcus.tan.598@gmail.com>
2022-11-02 12:11:54 +0800

    Add history service

---
+++ history-service/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { FieldValue } = require('firebase-admin/firestore');
+
+const app = express()
+const port = 8484
+const { db } = require('./firebase-config.js');
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World history-service');
+});
+
+app.post('/history', async (req, res) => {
+    const { username, questionID } = req.body
+    const userRef = db.collection('history').doc(username)
+    const res2 = await userRef.set({
+        "questionID" : FieldValue.arrayUnion(questionID)
+    }, { merge: true });
+})
+
+// get questions history for a particular username
+app.get('/history/:username', async (req, res) => {
+    const { username } = req.params
+    console.log('[Retrieve by username] ', username)
+    const docRef = db.collection('history').doc(username);
+    const snapshot = await docRef.get();
+
+    if (!username || snapshot.empty) {
+        return res.sendStatus(404)
+    }
+    
+    questionIdHistory = snapshot.data().questionID;
+
+    var qnsArray = [];
+
+    for (const id of questionIdHistory) {
+        const questionsRef = db.collection('questions').doc(id);
+        const snapshot2 = await questionsRef.get();
+        
+        if (snapshot2.data() != undefined) {
+            qnsArray.push([snapshot2.data()?.questionTitle, snapshot2.data()?.difficulty]);
+        }
+    }
+
+    res.status(200).send(qnsArray);
+})
+
+app.listen(port, () => console.log(`Server has started on port: ${port}`));
